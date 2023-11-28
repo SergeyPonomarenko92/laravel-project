b8d6192e1769ad2f5708590db051d4f6ff161b80
Ponomarenko <sergey.ponomarenko.stratis@gmail.com>
2023-11-28 23:36:14 +0200

    [Lesson2] exercise 4

--- app/Http/Controllers/api/RecursionController.php
+++ app/Http/Controllers/api/RecursionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use function Psy\debug;
 
 class RecursionController extends Controller
 {
@@ -26,6 +27,7 @@ class RecursionController extends Controller
     }
 
 
+
     public function fibonacci($index)
     {
         $result = $this->calculateFibonacci($index);
@@ -36,6 +38,21 @@ class RecursionController extends Controller
         ]);
     }
 
+
+    public function apples($pattern, $index)
+    {
+        $chain = [];
+        for ($i = 0; $i <= $index; $i++) {
+            $chain[$i] = $pattern;
+        }
+        $chain = implode($chain);
+        $chain = str_split($chain);
+        array_unshift($chain, ' ');
+
+        return $this->indexchecker($chain, $index);
+    }
+
+
     private function calculateFibonacci($index)
     {
         if ($index <= 1) {
@@ -43,6 +60,21 @@ class RecursionController extends Controller
         } else {
             return $this->calculateFibonacci($index - 1) + $this->calculateFibonacci($index - 2);
         }
+    }
+
+    private function indexchecker($chain, $index)
+    {
+        $result = [];
+        foreach ($chain as $key => $value  ) {
+            if ($index == $key){
+                $result = [
+                    'color' => "{$value}",
+                    'index' => "{$key}",
+                ];
+            }
+        }
+        return $result;
+
     }
 
 }
